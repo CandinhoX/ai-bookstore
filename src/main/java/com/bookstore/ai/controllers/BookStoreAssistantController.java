62a9c654571b2ab425d5595ac641f4fd35ff8123
candinho <can_dinho7@hotmail.com>
2024-04-15 14:32:10 -0300

    end of the project

--- src/main/java/com/bookstore/ai/controllers/BookStoreAssistantController.java
+++ src/main/java/com/bookstore/ai/controllers/BookStoreAssistantController.java
@@ -1,11 +1,13 @@
 package com.bookstore.ai.controllers;
 
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/bookstore")
@@ -15,8 +17,26 @@ public class BookStoreAssistantController {
     private OpenAiChatClient chatClient;
 
     @GetMapping("/informations")
-    public String bookstoreChat(@RequestParam(value = "message", defaultValue = "Quais os livros best sellers de 2024?")String message){
+    public String bookstoreChat(@RequestParam(value = "message",
+            defaultValue = "Quais são os livros best sellers dos ultimos anos?") String message){
         return chatClient.call(message);
+    }
+
+    @GetMapping("/reviews")
+    public String bookstoreReview(@RequestParam(value = "book", defaultValue = "Dom Quixote") String book) {
+        PromptTemplate promptTemplate = new PromptTemplate("""
+                  Por favor, me forneça
+                  um breve resumo do livro {book}
+                  e também a biografia de seu autor.
+                """);
+        promptTemplate.add("book", book);
+        return this.chatClient.call(promptTemplate.create()).getResult().getOutput().getContent();
+    }
+
+    @GetMapping("/stream/informations")
+    public Flux<String> bookstoreChatStream(@RequestParam(value = "message",
+            defaultValue = "Quais são os livros best sellers dos ultimos anos?") String message){
+        return chatClient.stream(message);
     }
 
 }
